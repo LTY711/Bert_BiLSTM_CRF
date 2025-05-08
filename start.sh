ecca55127667905feb345c8833e39b28b788e55a
Yoyo <522762176@qq.com>
2025-05-08 13:29:09 +0800

    测试提交修 (#2)
    Co-authored-by: yoyo <yoyo@yoyodeMacBook-Pro.local>

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-cd /Users/yoyo/Documents/2025/项目/Bert_BiLSTM_CRF_NER
+cd /Users/xx/Documents/2025/项目/Bert_BiLSTM_CRF_NER
 # 后台运行
-nohup streamlit run api.py > /Users/yoyo/Documents/2025/项目/Bert_BiLSTM_CRF_NER/logs/ner.log 2>&1 &
+nohup streamlit run api.py > /Users/xx/Documents/2025/项目/Bert_BiLSTM_CRF_NER/logs/ner.log 2>&1 &
